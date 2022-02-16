c018fd507d831fd43959e2556048ebe58f7c3289
SagarSrinivas <68839417+sagarsrinivas3@users.noreply.github.com>
2022-02-16 18:46:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import cv2
+import os
+
+def getImageArrayColor(image):
+  color = cv2.imread(image, 1) # 1 for color 0 for b&w
+  return color
+
+def getImageArrayGray(image):
+  color = cv2.imread(image, 0) # 1 for color 0 for b&w
+  return color
+
+def createGreyImage(image, rename):
+  color = cv2.imread(image, 0)
+  cv2.imwrite(rename, color)
+  
+def getImagesList(dir):
+  images = os.listdir(dir)
+  return images
+
+def convertMultipleImagesToGray(src, dest):
+  images = getImagesList(src)
+  for image in images:
+    createGreyImage(f'{src}/{image}', f'{dest}/gray-{image}')
+    #createGreyImage(src+"/"+image, dest+"/"+f'gray-{image}')
+    pass
+
+def getDimension(img):
+  image = cv2.imread(img)
+  return image.shape
+
+def getImage(img):
+  image = cv2.imread(img)
+  return image
+
+def getNewHeightAndWidth(scale_percentage, width, height):
+  new_height = int(height * scale_percentage / 100)
+  new_width = int(width * scale_percentage / 100)
+  return (new_width, new_height)
+
+def resize(img_path, scale_percent, resized_path):
+  shape = getDimension(img_path)
+  image = getImage(img_path)
+  new_shape = getNewHeightAndWidth(scale_percent, shape[1], shape[0])
+  resize_image = cv2.resize(image, new_shape)
+  cv2.imwrite(resized_path, resize_image)
+
+def DetectObjects(image_path, object_xml_path, newfilepath):
+  image = getImageArrayColor(image_path)
+  face_cascade = cv2.CascadeClassifier(object_xml_path)
+  faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=4)
+  # If faces len is 0 then faces are not detected
+  COLOR = (255,255,255)
+  WIDTH = 4
+  for (x,y,w,h) in faces:
+    cv2.rectangle(image, (x,y), (x+w, y+h), COLOR , WIDTH )
+  if len(faces) > 0 :
+    print(f"{len(faces)} Faces are not detected!!")
+    cv2.imwrite(newfilepath, image)
+    print("Output Image created!!")
+  else:
+    print("Faces are not detected!!")
+  
+#case 1 : get image array
+#print(getImageArray('galaxy.jpeg'))
+
+#case 2 : convert to gray image
+#createGreyImage("pictures/galaxy.jpeg", "gray-pictures/pictures/galaxy-gray.jpeg")
+
+#case 3 :convert multipe images to gray
+#convertMultipleImagesToGray("pictures", "gray-pictures")
+
+#case 4 : Resize the image
+#resize("pictures/galaxy.jpeg", 90, "resize-image.jpeg")
+
+#case 5 : Detect Faces
+#DetectObjects("faces.jpg", "config/faces.xml", "humanfaces.png")
+
+  
